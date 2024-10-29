135cdb3a0805de55b5fc90052dcb9e27fe715bea
EtherD3v <aymericduparc78@gmail.com>
2024-10-29 19:18:03 +0100

    chore/feat : redesign project root && develop main UI

---
+++ README.md
@@ -0,0 +1,23 @@
+# NerdITools
+> A collection of creative tools - fun first, useful sometimes!
+
+<div align="center">
+  
+  ![NerdITools preview](https://etherd3v.github.io/NerdITools/)
+  
+  [![License](https://img.shields.io/badge/license-MIT-blue.svg)](LICENSE)
+  
+</div>
+
+## Quick Start
+
+```bash
+# Clone the project
+git clone https://github.com/EtherD3v/NerdITools.git
+
+# Install dependencies
+npm install
+
+# Launch development server
+npm run dev
+
---
+++ src/counter.js
@@ -0,0 +1,40 @@
+export function setupCounter(element) {
+  let counter = 0
+  const setCounter = (count) => {
+    counter = count
+    element.innerHTML = `Answer : ${counter}`;
+    switch(counter){
+      case 0:
+        element.innerHTML = `CAPTCHA : please click here`;
+        break;
+      case 1:
+        element.innerHTML = `How many films has Christopher Nolan designed?`;
+        break;
+      case 12:
+        element.innerHTML = `What is the maximum number of players on a vanilla Minecraft server?`;
+        break;
+      case 20:
+        element.innerHTML = `What is the default port for HTTP protocol ?`;
+        break;
+      case 80:
+        element.innerHTML = `How many keys does a standart AZERTY keyboard have ?`;
+        break;
+      case 105:
+        element.innerHTML = `How many different colors can you get with 8 bits ? `;
+        break;
+      case 256:
+        if ("yes" === prompt("Do you fingers hurt?")){
+          alert("You're an human! :)")
+          element.innerHTML = `VERIFIED`;
+        }
+        else{
+          alert("You failed! :( \n Please Retry.")
+          element.innerHTML = `END`
+        }
+        alert(result)
+        break;
+    }
+  }
+  element.addEventListener('click', () => setCounter(counter + 1))
+  setCounter(0)
+}
