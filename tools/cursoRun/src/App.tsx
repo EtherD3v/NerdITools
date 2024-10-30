08bd9c1f20597c8076543eb5d0e4d387e46ffb00
EtherD3v <aymericduparc78@gmail.com>
2024-10-30 14:10:35 +0100

    chore: clean structure && progress on cursoRun

--- tools/cursoRun/src/App.tsx
+++ tools/cursoRun/src/App.tsx
@@ -1,6 +1,5 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
+import cursorIcon from './assets/cursor.svg'
 import './App.css'
 
 function App() {
@@ -9,24 +8,22 @@ function App() {
   return (
     <>
       <div>
-        <a href="https://vite.dev" target="_blank" rel="noreferrer">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank" rel="noreferrer">
-          <img src={reactLogo} className="logo react" alt="React logo" />
+        <a href="../README.md" target="_blank">
+          <img src={cursorIcon} className="logo" alt="cursor" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>CURSORUN</h1>
+      <h3>(Beta in development)</h3>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+          Spam clicks! {count}
         </button>
         <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
+          Train your cursor with <code><a href="https://doodles.google/doodle/halloween-2024/">this game.</a></code>
         </p>
       </div>
       <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+        Click on the cursor to learn more
       </p>
     </>
   )
