2ba0701f415082eb52c632ebdd014ec0fd0dffcf
EtherD3v <aymericduparc78@gmail.com>
2025-01-26 20:22:40 +0100

    feat: develop navigation bar template

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,13 @@
 import { useState } from 'react'
 import cursorIcon from './assets/cursor.svg'
 import './App.css'
+import ResponsiveAppBar from './components/AppBar.tsx'
 
-function App() {
-  const [count, setCount] = useState(0)
+const App: React.FC = () => {
 
   return (
     <>
-      <div>
-        <a href="../README.md" target="_blank">
-          <img src={cursorIcon} className="logo" alt="cursor" />
-        </a>
-      </div>
-      <h1>CURSORUN</h1>
-      <h3>(Beta in development)</h3>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          Spam clicks! {count}
-        </button>
-        <p>
-          Train your cursor with <code><a href="https://doodles.google/doodle/halloween-2024/">this game.</a></code>
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the cursor to learn more
-      </p>
+      <ResponsiveAppBar />
     </>
   )
 }
